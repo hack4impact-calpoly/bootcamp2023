5a4787b6efbf933ead5b5a5bd9a3db7aa198b06a
IvanA673 <22ivana04@gmail.com>
2023-10-18 22:22:55 -0700

    feat: create a list of Blog object.

---
+++ src/blogs.ts
@@ -0,0 +1,21 @@
+type Blog = {
+    title: string;
+    date: string;
+    description: string;
+    slug: string;
+}
+
+const blogs: Blog[] = [
+    {
+        title: "Sample Blog 1",
+        date: "2023-10-18",
+        description: "The first sample blog.",
+        slug: "sample-blog-1"
+    },
+    {
+        title: "Sample Blog 2",
+        date: "2023-10-19",
+        description: "The second sample blog.",
+        slug: "sample-blog-2"
+    }
+]
