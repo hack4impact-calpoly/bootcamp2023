54bc2b6fbb6f14f71b655692d83accf5d887c8df
Leila Touati <leilat014@gmail.com>
2023-10-21 14:59:43 -0700

    feat: add src

---
+++ src/blog.ts
@@ -0,0 +1,30 @@
+type Blog = {
+    title: string;
+    date: string;
+    desc: string;
+    slug: string;
+};
+
+const people: Blog[] = [
+    {
+        title: "Everything is Going Great",
+        date: "01-01-2002",
+        desc: "New Year's Day could not be going better.",
+        slug: "url"
+    },
+
+    {
+        title: "My Life in Shambles",
+        date: "01-02-2022",
+        desc: "The tough start to a new year.",
+        slug: "url",
+    },
+
+    {
+        title: "Back to Doing Better",
+        date: "-1-03-2022",
+        desc: "False alarm on that last post there, all is well again.",
+        slug: "url",
+    }
+];
+
