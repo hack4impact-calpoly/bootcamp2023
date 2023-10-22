773b66e3e0a06cd26e80adf31642050d7d31318b
Leila Touati <leilat014@gmail.com>
2023-10-22 12:48:02 -0700

    creation of js file

---
+++ src/blog.js
@@ -0,0 +1,28 @@
+"use strict";
+const blogs = [
+    {
+        title: "Everything is Going Great",
+        date: "01-01-2002",
+        desc: "New Year's Day could not be going better.",
+        slug: "url"
+    },
+    {
+        title: "My Life in Shambles",
+        date: "01-02-2022",
+        desc: "The tough start to a new year.",
+        slug: "url",
+    },
+    {
+        title: "Back to Doing Better",
+        date: "-1-03-2022",
+        desc: "False alarm on that last post there, all is well again.",
+        slug: "url",
+    }
+];
+blogs.forEach(item => {
+    const element = document.createElement('div');
+    const innerHTML = element.innerHTML = 'New blog piece';
+    const addingBlog = document.getElementById('blog');
+    addingBlog.appendChild(element);
+    console.log(addingBlog);
+});
