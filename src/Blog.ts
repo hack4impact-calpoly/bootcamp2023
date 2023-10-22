047796cb07b7eaed454628f68e96d4a21fefc2c8
Aidan Nesbitt <aidannesbitt20@gmail.com>
2023-10-22 13:41:49 -0700

    feat: added second blog Post in the blog List

--- src/Blog.ts
+++ src/Blog.ts
@@ -7,7 +7,12 @@ type Blog = {
 const blogs: Blog[] = [
   {
     date: "10/22/23",
-    description: "Welcome to my first Blog Post!",
+    description: "Welcome to my first Blog post!",
     slug: "firstBlogPost",
+  },
+  {
+    date: "10/22/23",
+    description: "This is my second Blog post!",
+    slug: "secondBlogPost",
   },
 ];
