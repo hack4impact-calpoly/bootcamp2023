de6304394737f32e37a4c2f4658f36c40e403443
Aidan Nesbitt <aidannesbitt20@gmail.com>
2023-10-22 14:11:41 -0700

    feat: transpiled .ts into .js file and added script link within blog.html

---
+++ src/blog.js
@@ -0,0 +1,12 @@
+var blogs = [
+    {
+        date: "10/22/23",
+        description: "Welcome to my first Blog post!",
+        slug: "firstBlogPost",
+    },
+    {
+        date: "10/22/23",
+        description: "This is my second Blog post!",
+        slug: "secondBlogPost",
+    },
+];
