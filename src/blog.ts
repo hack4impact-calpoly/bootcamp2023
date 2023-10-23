694cfafddb3e8bf064f63649eb12abd9a8141e41
aumerov <89824787+aumerov@users.noreply.github.com>
2023-10-22 19:24:30 -0700

    feat: Added a blog.ts file with a container and 2 examples

---
+++ src/blog.ts
@@ -0,0 +1,22 @@
+type Blog = {
+    title: string;
+    date: string;
+    description: string;
+    slug: string;
+};
+
+const blogs: Blog[] = [
+    {
+        title: "My First Blog",
+        date: "10-22-2023",
+        description: "This is my first blog! Lets continue coding :D",
+        slug: "my-first-blog",
+    },
+    {
+        title: "Why Star Wars is the best franchise",
+        date: "10-23-2023",
+        description: "Star Wars has cool ahh lightsabers and force weilding maniacs. Enough said?",
+        slug: "why-star-wars-is-the-best-franchise",
+    }
+];
+
