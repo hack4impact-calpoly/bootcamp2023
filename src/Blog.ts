f80157a7b0a7ce5f5c3fbe4e3ec2f4e53675a78e
unknown <kaylatran2021@gmail.com>
2023-10-23 12:28:16 -0700

    typescript object and list, js function

---
+++ src/Blog.ts
@@ -0,0 +1,21 @@
+type Blog = {
+  title: string;
+  date: string;
+  description: string;
+  slug: string;
+};
+
+const blogs: Blog[] = [
+  {
+    title: "Japan and Korea",
+    date: "03-20-2023",
+    description: "...",
+    slug: "",
+  },
+  {
+    title: "Thailand and Singapore",
+    date: "03-20-2023",
+    description: "...",
+    slug: "",
+  },
+];
