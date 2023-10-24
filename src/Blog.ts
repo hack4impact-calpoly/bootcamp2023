4aba7ccf069144a0df50468a7978980a74ae405c
Asarel Castellanos <asarelc@gmail.com>
2023-10-23 18:02:23 -0700

    finished milestone 1

--- src/Blog.ts
+++ src/Blog.ts
@@ -9,26 +9,30 @@ const blogs: Blog[] = [
   {
     title: "My first blog!",
     date: "Oct 1, 2023",
-    description: "This is my first ever blog! Welcome everyone!",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue lacus viverra vitae congue eu. Praesent elementum facilisis leo vel fringilla. Massa massa ultricies mi quis hendrerit dolor magna. Auctor elit sed vulputate mi sit amet.",
     slug: "first-blog",
   },
   {
     title: "My second blog!",
-    date: "Oct 1, 2023",
-    description: "This is my second ever blog! Welcome everyone!",
+    date: "Oct 2, 2023",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue lacus viverra vitae congue eu. Praesent elementum facilisis leo vel fringilla. Massa massa ultricies mi quis hendrerit dolor magna. Auctor elit sed vulputate mi sit amet.",
     slug: "second-blog",
   },
   {
     title: "My third blog!",
-    date: "Oct 1, 2023",
-    description: "This is my third ever blog! Welcome everyone!",
+    date: "Oct 3, 2023",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue lacus viverra vitae congue eu. Praesent elementum facilisis leo vel fringilla. Massa massa ultricies mi quis hendrerit dolor magna. Auctor elit sed vulputate mi sit amet.",
     slug: "third-blog",
   },
   {
     title: "My fourth blog!",
-    date: "Oct 1, 2023",
-    description: "This is my fourth ever blog! Welcome everyone!",
-    slug: "fifth-blog",
+    date: "Oct 4, 2023",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue lacus viverra vitae congue eu. Praesent elementum facilisis leo vel fringilla. Massa massa ultricies mi quis hendrerit dolor magna. Auctor elit sed vulputate mi sit amet.",
+    slug: "fourth-blog",
   },
 ];
 
@@ -39,8 +43,18 @@ function createBlogs() {
 
     // create the parent div and add a text node
     const parentDiv = document.createElement("div");
-    parentDiv.innerHTML =
-      blog.title + "\n" + blog.date + "\n" + blog.description;
+    parentDiv.className = "flex justify-center";
+    let link = "./blogs/" + blog.slug + ".html";
+    parentDiv.innerHTML = `
+        <div class="bg-[#344e41] w-[500px]">
+        <div class="flex justify-around pb-5">
+          <h2 class="text-white">${blog.title}</h2>
+          <p class="text-white">${blog.date}</p>
+        </div>
+        <p class="mb-5 text-white">${blog.description}</p>
+        <a class="bg-[#344e41] text-white underline" href="${link}">Check out the blog here!</a>
+        </div>
+        `;
 
     // insert parent div into the blog div
     blogDiv?.appendChild(parentDiv);
--- src/blog.js
+++ src/blog.js
@@ -2,26 +2,26 @@ var blogs = [
     {
         title: "My first blog!",
         date: "Oct 1, 2023",
-        description: "This is my first ever blog! Welcome everyone!",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue lacus viverra vitae congue eu. Praesent elementum facilisis leo vel fringilla. Massa massa ultricies mi quis hendrerit dolor magna. Auctor elit sed vulputate mi sit amet.",
         slug: "first-blog",
     },
     {
         title: "My second blog!",
-        date: "Oct 1, 2023",
-        description: "This is my second ever blog! Welcome everyone!",
+        date: "Oct 2, 2023",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue lacus viverra vitae congue eu. Praesent elementum facilisis leo vel fringilla. Massa massa ultricies mi quis hendrerit dolor magna. Auctor elit sed vulputate mi sit amet.",
         slug: "second-blog",
     },
     {
         title: "My third blog!",
-        date: "Oct 1, 2023",
-        description: "This is my third ever blog! Welcome everyone!",
+        date: "Oct 3, 2023",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue lacus viverra vitae congue eu. Praesent elementum facilisis leo vel fringilla. Massa massa ultricies mi quis hendrerit dolor magna. Auctor elit sed vulputate mi sit amet.",
         slug: "third-blog",
     },
     {
         title: "My fourth blog!",
-        date: "Oct 1, 2023",
-        description: "This is my fourth ever blog! Welcome everyone!",
-        slug: "fifth-blog",
+        date: "Oct 4, 2023",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In iaculis nunc sed augue lacus viverra vitae congue eu. Praesent elementum facilisis leo vel fringilla. Massa massa ultricies mi quis hendrerit dolor magna. Auctor elit sed vulputate mi sit amet.",
+        slug: "fourth-blog",
     },
 ];
 function createBlogs() {
@@ -30,8 +30,9 @@ function createBlogs() {
         var blogDiv = document.getElementById("blogs");
         // create the parent div and add a text node
         var parentDiv = document.createElement("div");
-        parentDiv.innerHTML =
-            blog.title + "\n" + blog.date + "\n" + blog.description;
+        parentDiv.className = "flex justify-center";
+        var link = "./blogs/" + blog.slug + ".html";
+        parentDiv.innerHTML = "\n        <div class=\"bg-[#344e41] w-[500px]\">\n        <div class=\"flex justify-around pb-5\">\n          <h2 class=\"text-white\">".concat(blog.title, "</h2>\n          <p class=\"text-white\">").concat(blog.date, "</p>\n        </div>\n        <p class=\"mb-5 text-white\">").concat(blog.description, "</p>\n        <a class=\"bg-[#344e41] text-white underline\" href=\"").concat(link, "\">Check out the blog here!</a>\n        </div>\n        ");
         // insert parent div into the blog div
         blogDiv === null || blogDiv === void 0 ? void 0 : blogDiv.appendChild(parentDiv);
     });
