04f75f0a3a29ea3f65e170139342ec872bee475c
Aidan Nesbitt <aidannesbitt20@gmail.com>
2023-10-23 17:14:47 -0700

    feat: created function that loops through blog list, and adds each blog element to the DOM

--- src/Blog.ts
+++ src/Blog.ts
@@ -1,5 +1,6 @@
 type Blog = {
   date: string;
+  title: string;
   description: string;
   slug: string;
 };
@@ -7,23 +8,44 @@ type Blog = {
 const blogs: Blog[] = [
   {
     date: "10/22/23",
+    title: "First Post!",
     description: "Welcome to my first Blog post!",
     slug: "firstBlogPost",
   },
   {
     date: "10/22/23",
+    title: "Second Post!",
     description: "This is my second Blog post!",
     slug: "secondBlogPost",
   },
 ];
 
 function addBlogsToHTML(blogs: Blog[]) {
+  var blogContainer = document.getElementById("blog-list");
 
-    var blogContainer = document.getElementById('blog-list');
+  if (blogContainer) {
+    blogs.forEach((blog) => {
+      const blogPost = document.createElement("div");
+      blogContainer?.classList.add("individual-blog-post"); // why need optional chaining here? if already checking for null?
 
-    blogs.forEach(blog => {
-        const blogPost = document.createElement('div');
-        blogPost.innerHTML = blog.description;
-        blogContainer.appendChild(blogPost);
-    })
-};
+      const blogTitle = document.createElement("h2");
+      blogTitle.classList.add("blog-post-title");
+
+      const blogDescription = document.createElement("p");
+      blogDescription.classList.add("blog-post-description");
+
+      const blogDate = document.createElement("p");
+      blogDate.classList.add("blog-post-date");
+
+      blogDescription.innerHTML = blog.description;
+      blogTitle.innerHTML = blog.title;
+      blogDate.innerHTML = blog.date;
+      blogPost.appendChild(blogTitle);
+      blogPost.appendChild(blogDescription);
+      blogPost.appendChild(blogDate);
+      blogContainer?.appendChild(blogPost); // why need optional chaining here? if already checking for null?
+    });
+  }
+}
+
+addBlogsToHTML(blogs);
--- src/blog.js
+++ src/blog.js
@@ -1,14 +1,37 @@
 var blogs = [
     {
         date: "10/22/23",
+        title: "First Post!",
         description: "Welcome to my first Blog post!",
         slug: "firstBlogPost",
     },
     {
         date: "10/22/23",
+        title: "Second Post!",
         description: "This is my second Blog post!",
         slug: "secondBlogPost",
     },
 ];
-
-
+function addBlogsToHTML(blogs) {
+    var blogContainer = document.getElementById("blog-list");
+    if (blogContainer) {
+        blogs.forEach(function (blog) {
+            var blogPost = document.createElement("div");
+            blogContainer === null || blogContainer === void 0 ? void 0 : blogContainer.classList.add("individual-blog-post"); // why need optional chaining here? if already checking for null?
+            var blogTitle = document.createElement("h2");
+            blogTitle.classList.add("blog-post-title");
+            var blogDescription = document.createElement("p");
+            blogDescription.classList.add("blog-post-description");
+            var blogDate = document.createElement("p");
+            blogDate.classList.add("blog-post-date");
+            blogDescription.innerHTML = blog.description;
+            blogTitle.innerHTML = blog.title;
+            blogDate.innerHTML = blog.date;
+            blogPost.appendChild(blogTitle);
+            blogPost.appendChild(blogDescription);
+            blogPost.appendChild(blogDate);
+            blogContainer === null || blogContainer === void 0 ? void 0 : blogContainer.appendChild(blogPost); // why need optional chaining here? if already checking for null?
+        });
+    }
+}
+addBlogsToHTML(blogs);
