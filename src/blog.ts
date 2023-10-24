44bf115e7dc1bcfa8f71011678df5a4ab448d90c
Leila Touati <leilat014@gmail.com>
2023-10-23 19:03:26 -0700

    added html pages for each blog

--- src/blog.ts
+++ src/blog.ts
@@ -10,21 +10,21 @@ const blogs: Blog[] = [
         title: "Everything is Going Great",
         date: "01-01-2002",
         desc: "New Year's Day could not be going better.",
-        slug: "url"
+        slug: "blog_piece1"
     },
 
     {
         title: "My Life in Shambles",
         date: "01-02-2022",
         desc: "The tough start to a new year.",
-        slug: "url",
+        slug: "blog_piece2",
     },
 
     {
         title: "Back to Doing Better",
         date: "-1-03-2022",
         desc: "False alarm on that last post there, all is well again.",
-        slug: "url",
+        slug: "blog_piece3",
     }
 ];
 
@@ -38,8 +38,8 @@ function blogDisplay() {
         const blogPrev = document.createElement("div");
         blogPrev.classList.add("story-preview");
         blogPrev.innerHTML = 
-            `<h2 class = "post-title"> ${title} </h2>
-            <h3 class = "post-date"> ${date} </h3>
+            `<h3 class = "post-title"> ${title} </h3>
+            <h4 class = "post-date"> ${date} </h4>
             <p class = "post-desc"> ${desc} </p>`;
     
         blogPrev.appendChild(element);
