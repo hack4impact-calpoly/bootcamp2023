5de10a4fc4b50a1c3126f7f2053712b868ad4123
Sriya-Adunur <sadunur@calpoly.edu>
2023-10-24 21:36:18 -0700

    Oct 24

--- src/blog.js
+++ src/blog.js
@@ -3,13 +3,13 @@ var blogs = [
         title: "The Competitive Space of Grace Hopper",
         date: "10-23-2023",
         description: "My experience as a Grace Hopper attendee, this year.",
-        slug: "grace-hopper"
+        slug: "blog1",
     },
     {
         title: "Must Watch Halloween Movies",
         date: "10-24-2023",
         description: "A complete list of spooky films to watch for the holiday.",
-        slug: "halloween-movies"
+        slug: "blog2",
     },
 ];
 function blogPreview() {
@@ -18,7 +18,7 @@ function blogPreview() {
         blogs.forEach(function (blog) {
             var element = document.createElement("div");
             element.classList.add("blog-item");
-            element.innerHTML = "\n                <h2><a href=\"".concat(blog.slug, ".html\">").concat(blog.title, "</a></h2>\n                <p>").concat(blog.date, "</p>\n                <p>").concat(blog.description, "</p>\n            ");
+            element.innerHTML = "\n      <h2><a href=\"".concat(blog.slug, ".html\">").concat(blog.title, "</a></h2>\n      <p>").concat(blog.date, "</p>\n      <p>").concat(blog.description, "</p>\n    ");
             container.appendChild(element);
         });
     }
--- src/blog.ts
+++ src/blog.ts
@@ -10,13 +10,13 @@ const blogs: Blog[] = [
         title: "The Competitive Space of Grace Hopper",
         date: "10-23-2023",
         description: "My experience as a Grace Hopper attendee, this year.",
-        slug: "grace-hopper"
+        slug: "blog1",
     },
     {
         title: "Must Watch Halloween Movies",
         date: "10-24-2023",
         description: "A complete list of spooky films to watch for the holiday.",
-        slug: "halloween-movies"
+        slug: "blog2",
     },
 ];
 
@@ -29,11 +29,11 @@ function blogPreview()
       const element = document.createElement("div");
       element.classList.add("blog-item");
       element.innerHTML = `
-                <h2><a href="${blog.slug}.html">${blog.title}</a></h2>
-                <p>${blog.date}</p>
-                <p>${blog.description}</p>
-            `;
-      container.appendChild(element);
+      <h2><a href="${blog.slug}.html">${blog.title}</a></h2>
+      <p>${blog.date}</p>
+      <p>${blog.description}</p>
+    `; 
+    container.appendChild(element);
     });
   }
 }
