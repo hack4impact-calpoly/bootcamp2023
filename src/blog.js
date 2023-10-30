865e11753347c83e1988dc7b0f87a0e8ac271a5f
HanYu Wu <hanyuwu04@gmail.com>
2023-10-30 02:07:17 -0700

    feat: created and finished blog.js

---
+++ src/blog.js
@@ -0,0 +1,14 @@
+var blogs = [
+    {
+        title: "First Blog",
+        date: "2023-10-10",
+        description: "This is the first blog description.",
+        slug: "first-blog",
+    },
+    {
+        title: "Second Blog",
+        date: "2023-10-15",
+        description: "This is the second blog description.",
+        slug: "second-blog",
+    },
+];
