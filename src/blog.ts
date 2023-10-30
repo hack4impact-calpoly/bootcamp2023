b9e57219a7f3542bc184a400d15e564f53529002
kbecke05 <kbecke05@calpoly.edu>
2023-10-29 19:07:35 -0700

    feat: link main blog page to entries

--- src/blog.ts
+++ src/blog.ts
@@ -2,7 +2,7 @@ type Blog = {
     title: string;
     date: Date;
     description: string;
-    slug: URL;
+    slug: string;
 }
 
 const blogs: Blog[] = [
@@ -10,42 +10,23 @@ const blogs: Blog[] = [
         title: "Blog 1",
         date: new Date(),
         description: "My first blog post!",
-        slug: new URL("http://www.google.com")
+        slug: "blogentry1.html"
     },
     {
         title: "Blog 2",
         date: new Date(),
         description: "My second blog post!",
-        slug: new URL("http://www.google.com")
+        slug: "blogentry2.html"
     }
 ]
 
-/*function addBlogEntries() = {
-    const container = document.getElementById("blog-entries");
-
-    blogs.forEach(blog => {
-        const newBlog = document.createElement('div');
-        newBlog.className = "blog-entry";
-        const formattedDate = blog.date.toLocaleDateString();
-
-        newBlog.innerHTML = `
-        <h2>${blog.title}</h2>
-        <p>Date: ${formattedDate}</p>
-        <p>Description: ${blog.description}</p>
-        <p>Link: <a href="${blog.slug}" target="_blank">${blog.slug}</a></p>
-        `;
-
-        container.appendChild(newBlog);
-    })
-}*/
-
 function addBlogEntries(): void {
-    const container: HTMLElement | null = document.getElementById("blog-entries");
+    const container: HTMLElement | null = document.getElementById("entry");
 
     if (container) {
         blogs.forEach(blog => {
             const newBlog = document.createElement('div');
-            newBlog.className = "blog-entry";
+            newBlog.className = "entry";
             const formattedDate = blog.date.toLocaleDateString();
 
             newBlog.innerHTML = `
