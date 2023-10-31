08dcc9ebe3289c2941d5a8fc4fdfd53dde4d0bd9
Asarel Castellanos <asarelc@gmail.com>
2023-10-31 14:44:21 -0700

    relocated resume page to nextjs project

--- bootcamp-milestone-2/src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <main>
-        <h1>Footer Component</h1>
-      </main>
-    )
-  }
-  
--- bootcamp-milestone-2/src/components/Navbar.tsx
+++ bootcamp-milestone-2/src/components/Navbar.tsx
@@ -1,8 +1,26 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return (
-      <main>
-        <h1>Navbar Component</h1>
-      </main>
+      <nav className="text-white">
+        <ul>
+          <li className="text-2xl hover:underline py-1.5 hover:scale-125">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="text-2xl hover:underline py-1.5 hover:scale-125">
+            <Link href="/blogs">Blog</Link>
+          </li>
+          <li className="text-2xl hover:underline py-1.5 hover:scale-125">
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li className="text-2xl hover:underline py-1.5 hover:scale-125">
+            <Link href="/resume">Resume</Link>
+          </li>
+          <li className="text-2xl hover:underline py-1.5 hover:scale-125">
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     )
   }
   
