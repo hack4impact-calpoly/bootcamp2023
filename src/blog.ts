e251c0101e0dee25f31e2ee6b527d89d8b5e5e62
Keila Mohan <keilamohan@hotmail.com>
2023-10-31 16:59:30 -0700

    feat: add blog pages/ts file

--- src/blog.ts
+++ src/blog.ts
@@ -2,41 +2,48 @@ type Blog = {
 	title: string;
 	date: string;
 	description: string;
+    slug: string
 };
 
 const bloglist: Blog[] = [
 	{
-		title: "Hack4Impact Starter Pack",
-		date: "10/20/2023",
-		description: "I finished my Hack4Impact Starter Pack",
+		title: "My Internship @ Microchip Technology!",
+		date: "9/15/2023",
+		description: "I spent this past summer (2023) interning at Microchip Technology Inc. where I was able to apply the skills I learned in school to contribute to meaningful work.",
+        slug: "Blog1.html",
 	},
 	{
-		title: "Hack4Impact Milestone 1",
+		title: "Starting as a Software Dev @ Hack4Impact",
 		date: "10/29/2023",
-		description: "I began my Hack4Impact Milestone 1!",
+		description: "I began my position as a software developer at Hack4Impact, a 501c3 non-profit organization that develops software projects for local non-profits in San Luis Obispo County!",
+        slug: "Blog2.html"
 	},
 ];
 
-export function displayBlogEntries(){
-    const container = document.getElementById("container");
-    bloglist.forEach(blog => {
-        const blogEntry = document.createElement("div");
-        blogEntry.className = "blog-entry";
 
-        const title = document.createElement("h2");
-        title.textContent = blog.title;
+const container = document.getElementById("container");
+bloglist.forEach(blog => {
+    const blogEntry = document.createElement("div");
+    blogEntry.className = "blog-entry";
 
-        const date = document.createElement("p");
-        date.textContent = `Date: ${blog.date}`;
+    const title = document.createElement("h2");
+    title.textContent = blog.title;
 
-        const description = document.createElement("p");
-        description.textContent = blog.description;
+    const date = document.createElement("p");
+    date.textContent = `Date: ${blog.date}`;
 
-        blogEntry.appendChild(title);
-        blogEntry.appendChild(date);
-        blogEntry.appendChild(description);
+    const description = document.createElement("p");
+    description.textContent = blog.description;
 
+    const slug = document.createElement("a");
+    slug.href = blog.slug;
+    slug.textContent = "Visit this blog!";
+
+    blogEntry.appendChild(title);
+    blogEntry.appendChild(date);
+    blogEntry.appendChild(description);
+    blogEntry.appendChild(slug);
+    if (container){
         container.appendChild(blogEntry);
-    })
-
-}
+    }
+})
