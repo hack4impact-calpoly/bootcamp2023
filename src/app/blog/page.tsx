458f6982db449a7bdb69f958b16f88efef019b86
AceOfRay <122914105+AceOfRay@users.noreply.github.com>
2023-11-02 23:32:48 -0700

    Add files via upload

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+
+import blogStyles from "@/app/blog/blog.module.css"
+
+export default function Blog () {
+    return (
+        <div>
+            <h1 className={blogStyles.header}>This is the blog page</h1>
+        </div>
+    )
+}
