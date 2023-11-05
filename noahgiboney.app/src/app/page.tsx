f2d3f9fe7069b0a03d103c411307deee862e3afd
noahgiboney <noahgiboney@gmail.com>
2023-11-04 18:33:58 -0700

    feat: added footer to page layout

--- noahgiboney.app/src/app/page.tsx
+++ noahgiboney.app/src/app/page.tsx
@@ -1,8 +1,26 @@
-import styles from './page.module.css'
+// import linkedinIcon from './images/icons/linkedin.png'; // Adjust the path as needed
+// import githubIcon from './images/icons/github.png';    // Adjust the path as needed
+// import mailIcon from './images/icons/mail.png';       // Adjust the path as needed
+import  Style  from './page.module.css';
 
 export default function Home() {
   return (
-    <div>
+    <div id={Style.particleJs}>
+      <section className={Style.homeContainer}>
+        <h2>Noah Giboney</h2>
+        <div id={Style.line}></div>
+        <div className={Style.socialsContainer}>
+          <a href="https://www.linkedin.com/in/noah-giboney-896847261/" target="_blank" rel="noopener noreferrer">
+            <img src={'./images/icons/linkedin.png'} id="round" alt="LinkedIn" />
+          </a>
+          <a href="https://github.com/noahgiboney" target="_blank" rel="noopener noreferrer">
+            <img src={'./images/icons/github.png'} alt="GitHub" />
+          </a>
+          <a href="#contact">
+            <img src={'./images/icons/mail.png'} alt="Email" />
+          </a>
+        </div>
+      </section>
     </div>
   )
 }
---
+++ noahgiboney.app/src/components/Footer/footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./footer.module.css";
+
+export default function Footer() {
+  return (
+   <footer className={style.footer}>
+        <p>noahgiboney © 2023</p>
+                {/* <a href="https://www.linkedin.com/in/noah-giboney-896847261/"><img src={"images/icons/linkedin.png"} id="round"></a>
+                <a href="https://github.com/noahgiboney"><img src={"images/icons/github.png"} id="round"></a> */}
+    </footer>
+  );
+}
