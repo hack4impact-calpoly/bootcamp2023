809a77e090c78c6407f4c13f955a0752908396d4
Bolt259 <sakul2003@gmail.com>
2023-11-05 15:55:00 -0800

    feat: removed unneccesary functions

--- src/blog.js
+++ src/blog.js
@@ -1,16 +1,3 @@
-// interface Blog {
-//     title: string;    
-//     date: string;
-//     description: string;
-//     slug: string;
-//     [key: string]: any
-// }
-// const blogTypes = [
-//     'Personal',
-//     'Career',
-//     'Exploration',
-//     'Tech',
-// ];
 var blogs = [
     {
         title: "Personal",
@@ -37,29 +24,6 @@ var blogs = [
         slug: "blog-tech",
     },
 ];
-// function createBlogElements() {
-//     const container = document.querySelector('.blog-container');
-//     if (container) {
-//         blogs.forEach(blog => {
-//             // Create a new <div> element for each blog
-//             const blogElement = document.createElement('div');
-//             blogElement.classList.add('blog-entry'); // Add a CSS class for styling
-//             // Create HTML content for the blog entry
-//             blogElement.innerHTML = `
-//                 <h2>${blog.title}</h2>
-//                 <p>Date: ${blog.date}</p>
-//                 <p>${blog.description}</p>
-//                 <a href="${blog.slug}">Read more</a>
-//             `;
-//             // Append the new blog element to the container
-//             container.appendChild(blogElement);
-//         });
-//     } else {
-//         console.error('Container not found.');
-//     }
-// }
-// // Call the function to create and append blog elements
-// createBlogElements();
 function displayBlogPreview() {
     // Sort the blog previews by date
     blogs.sort(function (a, b) {
--- src/blog.ts
+++ src/blog.ts
@@ -1,24 +1,9 @@
-// interface Blog {
-//     title: string;    
-//     date: string;
-//     description: string;
-//     slug: string;
-//     [key: string]: any
-// }
-
 type Blog = {
     title: string;    
     date: string;
     description: string;
     slug: string;
 }
-
-// const blogTypes = [
-//     'Personal',
-//     'Career',
-//     'Exploration',
-//     'Tech',
-// ];
 
 const blogs: Blog[] = [
     {
@@ -46,34 +31,6 @@ const blogs: Blog[] = [
         slug: "blog-tech",
     },
 ];
-
-// function createBlogElements() {
-//     const container = document.querySelector('.blog-container');
-
-//     if (container) {
-//         blogs.forEach(blog => {
-//             // Create a new <div> element for each blog
-//             const blogElement = document.createElement('div');
-//             blogElement.classList.add('blog-entry'); // Add a CSS class for styling
-
-//             // Create HTML content for the blog entry
-//             blogElement.innerHTML = `
-//                 <h2>${blog.title}</h2>
-//                 <p>Date: ${blog.date}</p>
-//                 <p>${blog.description}</p>
-//                 <a href="${blog.slug}">Read more</a>
-//             `;
-
-//             // Append the new blog element to the container
-//             container.appendChild(blogElement);
-//         });
-//     } else {
-//         console.error('Container not found.');
-//     }
-// }
-
-// // Call the function to create and append blog elements
-// createBlogElements();
 
 function displayBlogPreview() {
     // Sort the blog previews by date
