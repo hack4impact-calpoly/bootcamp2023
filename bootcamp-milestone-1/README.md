2be9eea703c48ffa5b93d9bb08bf91dc41566a71
drdonv <realdrdonv@gmail.com>
2023-11-05 12:54:00 -0800

    Redid the project based on feedback, add link to js file and list of blog pages. Apologies for the error the first time

---
+++ bootcamp-milestone-1/README.md
@@ -0,0 +1,4 @@
+# Dhanvi Ganti's Personal Website
+This is a personal website built using the Hack4Impact Starter Pack!
+
+Visit it here! https://drdonv.github.io
---
+++ bootcamp-milestone-2/src/app/blogData.ts
@@ -0,0 +1,20 @@
+export interface Blog {
+    
+}
+
+const blogs: Blog[] = [
+	{
+		title: "Tip 2",
+		date: "10-22-2023",
+		description: "Don't panic if you're broke - get money.",
+        slug: "blog2",
+	},
+	{
+		title: "Tip 1",
+		date: "10-21-2023",
+		description: "Don't become broke.",
+        slug: "blog1",
+	},
+];
+
+export default blogs; // Allows access to data anywhere
