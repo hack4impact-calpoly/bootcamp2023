52671f85c50cb90e06f7931f27982b85cff71ff2
aarav27 <aaravsharma927@gmail.com>
2023-11-05 15:12:37 -0800

    Styling Edits

--- milestone-2/src/app/blog/page.tsx
+++ milestone-2/src/app/blog/page.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import BlogPreview from '@/app/components/blogPreview';
 import blogs from "../blogData";
+import "../styles/website.css"
 
 export default function Blog() {
     return(
+    <main>
+        <h2 className="page-title">Blogs</h2>
         <div>
             {blogs.map(blog => 
                 <BlogPreview  {...blog} />
 		    )}
         </div>
+    </main>
         
     );
 
--- milestone-2/src/app/components/blogPreview.tsx
+++ milestone-2/src/app/components/blogPreview.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import type {Blog} from "../blogData";
-import Link from 'next/link';
 import style from './blogPreview.module.css';
 
 export default function BlogPreview(props: Blog) {
