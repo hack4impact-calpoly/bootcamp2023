bcd29bb145c35cbf4fede3479bf824ddc71eb782
Sean Nguyen <smnguyen745@gmail.com>
2023-11-06 04:24:18 +0100

    feat: removed useless code from blogData.ts

--- bootcamp-milestone-2/src/app/blogData.ts
+++ bootcamp-milestone-2/src/app/blogData.ts
@@ -20,32 +20,32 @@ const blogList: Blog[] = [
   },
 ];
 
-document.addEventListener("DOMContentLoaded", function () {
-  const parent = document.querySelector("#blog-list");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const parent = document.querySelector("#blog-list");
 
-  blogList.forEach((blog) => {
-    // container representing a row that will hold the blog post info
-    const createDiv = document.createElement("div");
-    createDiv.className = "blog-entry";
+//   blogList.forEach((blog) => {
+//     // container representing a row that will hold the blog post info
+//     const createDiv = document.createElement("div");
+//     createDiv.className = "blog-entry";
 
-    const createTitle = document.createElement("a"); // link to blog post
-    createTitle.href = blog.slug;
-    createTitle.textContent = blog.title;
+//     const createTitle = document.createElement("a"); // link to blog post
+//     createTitle.href = blog.slug;
+//     createTitle.textContent = blog.title;
 
-    const createDate = document.createElement("p"); // date blog post created
-    createDate.textContent = blog.date;
+//     const createDate = document.createElement("p"); // date blog post created
+//     createDate.textContent = blog.date;
 
-    const createDescription = document.createElement("p"); // brief description of blog post
-    createDescription.textContent = blog.description;
+//     const createDescription = document.createElement("p"); // brief description of blog post
+//     createDescription.textContent = blog.description;
 
-    // add all the elements to the container row
-    createDiv.append(createTitle);
-    createDiv.append(createDate);
-    createDiv.append(createDescription);
-    if (parent) {
-      parent.append(createDiv);
-    }
-  });
-});
+//     // add all the elements to the container row
+//     createDiv.append(createTitle);
+//     createDiv.append(createDate);
+//     createDiv.append(createDescription);
+//     if (parent) {
+//       parent.append(createDiv);
+//     }
+//   });
+// });
 
 export default blogList;
