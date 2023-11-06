61f575095892fb7ebdb4b84ed93c97a92a3fc5d9
Sean Nguyen <smnguyen745@gmail.com>
2023-11-06 04:36:12 +0100

    feat: added contact and portfolio link

--- bootcamp-milestone-2/src/components/navbar.tsx
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -8,13 +8,13 @@ export default function Navbar() {
     // with your navbar code from your earlier milestones
     <header className={style.navHead} >
       <nav className={style.navbar}>
-      <h1 className={style.logo}><Link href="index.html">Sean M. Nguyen</Link></h1>
+      <h1 className={style.logo}><Link href="/">Sean M. Nguyen</Link></h1>
       <ul className={style.navList}>
         <li><Link href="/">Home</Link></li>
         <li><Link href="/blog">Blog</Link></li> 
-        <li><Link href="portfolio.html">Portfolio</Link></li>
+        <li><Link href="/portfolio">Portfolio</Link></li>
         <li><Link href="/resume">Resume</Link></li>
-        <li><Link href="contact.html">Contact</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
       </ul>
     </nav>
     </header>
