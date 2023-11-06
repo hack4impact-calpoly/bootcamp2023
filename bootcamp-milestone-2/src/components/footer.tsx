28a2db4c1ec37484552aae35a72c714a00a51743
James <jamesktorres2022@gmail.com>
2023-11-06 13:33:13 -0800

    feat: updated footer message

--- bootcamp-milestone-2/src/components/footer.tsx
+++ bootcamp-milestone-2/src/components/footer.tsx
@@ -4,7 +4,7 @@ import style from "./footer.module.css";
 export default function Footer() {
   return (
     <div>
-        <footer className={style.Footer}>© 2023 My First Website: James' Junction | All Rights Reserved</footer>
+        <footer className={style.Footer}>© 2023 James' First Website | All Rights Reserved</footer>
     </div>
   );
 }
