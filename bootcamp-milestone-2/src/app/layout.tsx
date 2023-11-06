fb17089008f6f8c7748cdf8f09558ee866561b28
Sean Nguyen <smnguyen745@gmail.com>
2023-11-06 02:27:02 +0100

    feat: add layout.tsx and followed milestone 2 instructions

--- bootcamp-milestone-2/src/app/layout.tsx
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "../components/navbar";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sean M. Nguyen Personal Website",
   description: "A personal website for Sean M. Nguyen",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
