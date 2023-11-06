3af3e91d925fe775c87eaad68e20a5769d4fc96a
Leila Touati <leilat014@gmail.com>
2023-11-06 11:23:39 -0800

    error with Link, could not find so changed to <a tags, quick fixes for react error

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-console.log(React);
 import style from "./navbar.module.css";
 
 export default function Navbar() {
   return (
     // replace everything in between the <header> & <header /> tags
     // with your navbar code from your earlier milestones
-    <header className={style.[whatever class name you put]} >
+    <header className={style.navbar} >
       <h1> Leila's Personal Website </h1>
       <nav>
-        <Link href="/" >Home</Link>
-        <Link href="/about" >About Me</Link>
-				<Link href="/resume" >Resume</Link>
+        <a href="/" >Home</a>
+        <a href="/about" >About Me</a>
+				<a href="/resume" >Resume</a>
       </nav>
     </header>
   );
