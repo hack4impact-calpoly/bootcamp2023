4b7943eab20487c82f267f1ac4f62e8503dc4c1b
James <jamesktorres2022@gmail.com>
2023-11-05 22:58:15 -0800

    feat: convert footer to react

---
+++ bootcamp-milestone-2/src/components/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <div>
+        <footer className={style.Footer}>© 2023 My First Website: James' Junction | All Rights Reserved</footer>
+    </div>
+  );
+}
