60c9079aa9100161495c4eb30762cfdace42deff
brandonwDiaryz <73367549+wbrandon25@users.noreply.github.com>
2023-11-05 23:23:44 -0800

    feat: add routes and navigation fix

--- bootcamp-milestone-2/src/components/Navbar.tsx
+++ bootcamp-milestone-2/src/components/Navbar.tsx
@@ -7,17 +7,18 @@ export default function Navbar() {
         // replace everything in between the <header> & <header /> tags
         // with your navbar code from your earlier milestones
         <header className={`${style.navbar} ${style.navList} }`} >
-            
+
             <nav className="navbar">
-        <h1 className="logo"><a href="index.html">Brandon's Personal Website</a></h1>
-        <ul className="nav-list">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="blog.html">Blog</a></li>
-            <li><a href="portfolio.html">Portfolio</a></li>
-            <li><a href="resume.html">Resume</a></li>
-            <li><a href="contact.html">Contact</a></li>
-        </ul>
-    </nav>
+                <h1 className="logo"><a href="index.html">Brandon's Personal Website</a></h1>
+                <ul className="nav-list">
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/blog">Blog</Link></li>
+                    <li><Link href="/portfolio">Portfolio</Link></li>
+                    <li><Link href="/resume">Resume</Link></li>
+                    <li><Link href="/contact">Contact</Link></li>
+
+                </ul>
+            </nav>
         </header>
-    );
+    ); 
 }
