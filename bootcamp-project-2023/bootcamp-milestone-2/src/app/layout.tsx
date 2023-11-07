6a86dff47113d91cb302782f577196552a2a980a
Aidan Nesbitt <aidannesbitt20@gmail.com>
2023-11-07 13:23:05 -0800

    feat: added footer, having styling issues where there are gaps between the footer and the bottom of the page

--- bootcamp-project-2023/bootcamp-milestone-2/src/app/layout.tsx
+++ bootcamp-project-2023/bootcamp-milestone-2/src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google"; // You can change the font to anything you want.
 import Navbar from "../components/navbar";
-
+import Footer from "../components/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <Navbar />
       <body className={inter.className}>{children}</body>
+      <Footer />
     </html>
   );
 }
