55c25a72e753c41360581fc744910d682da805ef
Sohini-pillay <sohinispillay@gmail.com>
2023-11-08 10:12:13 -0800

    feat: add footer component

---
+++ bootcamp-milestone-2/app/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import style from "./footer.module.css";
+
+export default function Footer() {
+    return (
+        <footer className={style.footer}>© 2023 Sohini's Personal Website | All Rights Reserved</footer>
+    );
+}
+
