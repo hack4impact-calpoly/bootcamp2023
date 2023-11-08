538b88df108078e029ad0838d2e10ff98a9cbf55
Sohini-pillay <sohinispillay@gmail.com>
2023-11-08 10:11:21 -0800

    feat: add blogPreview component

---
+++ bootcamp-milestone-2/app/components/blogPreview.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import type { Blog } from "../blogData";
+import style from "./blogPreview.module.css";
+
+export default function BlogPreview(props: Blog) {
+  return (
+    <div className={style.blog}>
+      <div>
+        <h3>{props.title}</h3>
+        <p>{props.date}</p>
+        <p>{props.description}</p>
+      </div>
+    </div>
+  );
+}
