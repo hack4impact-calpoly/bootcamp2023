9bbbcdb515b8c79ca1e9c7846058728e804d3df4
Leila Touati <leilat014@gmail.com>
2023-11-08 22:34:12 -0800

    created blogData file, added blog list

---
+++ bootcamp-milestone-2/src/blogData.ts
@@ -0,0 +1,27 @@
+export interface Blog {
+
+}
+const blogs: Blog[] = [
+    {
+        title: "Hello there",
+        date: "10-18-2023",
+        desc: "A slight introduction",
+        slug: "blog_piece1"
+    },
+
+    {
+        title: "Another Day another Sunset",
+        date: "10-19-2023",
+        desc: "The origins of the sloset",
+        slug: "blog_piece2",
+    },
+
+    {
+        title: "Visiting Seattle",
+        date: "10-20-2023",
+        desc: "Time well spent in an awesome city",
+        slug: "blog_piece3",
+    }
+];
+
+export default blogs;
