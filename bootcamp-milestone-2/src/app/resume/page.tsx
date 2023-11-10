ae2242e8b213cb54869bf8f50e50acf35951de80
brandonwDiaryz <73367549+wbrandon25@users.noreply.github.com>
2023-11-10 07:53:37 -0800

    refactor: create component for resume entries

--- bootcamp-milestone-2/src/app/resume/page.tsx
+++ bootcamp-milestone-2/src/app/resume/page.tsx
@@ -1,5 +1,5 @@
 import style from "./page.module.css";
-
+import resumeEntryList from "../resumeData";
 
 export default function Resume() {
     return (
@@ -10,50 +10,15 @@ export default function Resume() {
                 <a href="PlaceholderPage.pdf" download className="text-white"> Download Resume </a>
 
                 <div className="resume">
-                    <section className="section">
-                        <h2 className="section-title">
-                            Education
-                        </h2>
-                        <div className="entry">
-                            <h3 className="entry-title">California Polytechnic State University, San Luis Obispo</h3>
-                            <p className="entry-info">Bachelor of Science in Computer Science - Expected Graduation: June 2026</p>
-                            <p className="entry-description"></p>
-                        </div>
-                    </section>
-                    <section className="section">
-                        <h2 className="section-title">
-                            Experience
-                        </h2>
-                        <div className="entry">
-                            <h3 className="entry-title">Software Developer Intern</h3>
-                            <p className="entry-info">Diaryz | June 2023 – Present</p>
-                            <p className="entry-description">Developed RESTful APIs using Django’s Rest Framework to communicate
-                                with a React Native app</p>
-                        </div>
-                        <div className="entry">
-                            <h3 className="entry-title">Full Stack Web Developer Intern </h3>
-                            <p className="entry-info">The Commons XR | December 2022 – May 2023</p>
-                            <p className="entry-description">Created a Python backend server to manage user data collection,
-                                visualization, and Unity game integration</p>
-                        </div>
-                    </section>
-                    <section className="section">
-                        <h2 className="section-title">
+                    {resumeEntryList.map(entry =>
 
-                            Projects
-                        </h2>
                         <div className="entry">
-                            <h3 className="entry-title">Hack4Impact Starter Pack</h3>
-                            <p className="entry-info">Developed a personal website using HTML and CSS</p>
-                            <p className="entry-description"></p>
+                            <h3 className="entry-title">{entry.entryTitle}</h3>
+                            <p className="entry-info">{entry.entryInfo}</p>
+                            <p className="entry-description">{entry.entryDescription}</p>
                         </div>
-                        <div className="entry">
-                            <h3 className="entry-title">Crash Gambling Simulator</h3>
-                            <p className="entry-info">Developed a real-time full-stack online multiplayer gambling game with a React
-                                frontend</p>
-                            <p className="entry-description"></p>
-                        </div>
-                    </section>
+                    )}
+
                     <section className="section">
                         <h2 className="section-title">
                             Coursework
---
+++ bootcamp-milestone-2/src/app/resumeData.ts
@@ -0,0 +1,31 @@
+type ResumeEntry = {
+    entryTitle: string;
+    entryInfo: string;
+    entryDescription: string;
+}
+
+const resumeEntryList: ResumeEntry[] = [
+    {
+        entryTitle: "California Polytechnic State University, San Luis Obispo",
+        entryInfo: "Bachelor of Science in Computer Science - Expected Graduation: June 2026",
+        entryDescription: "",
+    },
+    {
+        entryTitle: "Full Stack Web Developer Intern",
+        entryInfo: "The Commons XR | December 2022 – May 2023",
+        entryDescription: "Created a Python backend server to manage user data collection, visualization, and Unity game integration",
+    },
+    {
+        entryTitle: "Hack4Impact Starter Pack",
+        entryInfo: "Developed a personal website using HTML and CSS",
+        entryDescription: "",
+    },
+    {
+        entryTitle: "Crash Gambling Simulator",
+        entryInfo: "Developed a real-time full-stack online multiplayer gambling game with a React frontend",
+        entryDescription: "",
+    },
+    
+]
+
+export default resumeEntryList
