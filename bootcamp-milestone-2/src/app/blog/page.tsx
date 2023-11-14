fe779da8a50c7b7db94b21797daecf4cee9ed4c3
ruiyichi <danielching2@gmail.com>
2023-11-13 21:26:20 -0800

    feat: add db

--- bootcamp-milestone-2/src/app/blog/page.tsx
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -1,8 +1,24 @@
 import BlogPreview from "@/components/blogPreview";
-import blogs from "../blogData";
+import connectDB from "@/helpers/db";
+import Blog from "@/database/blogSchema";
 
-export default function BlogPreviews() {
-  return (
+async function getBlogs(){
+	await connectDB() // function from db.ts before
+
+	try {
+			// query for all blogs and sort by date
+	    const blogs = await Blog.find().sort({ date: -1 }).orFail()
+			// send a response as the blogs as the message
+	    return blogs
+	} catch (err) {
+	    return null
+	}
+}
+
+export default async function BlogPreviews() {
+  const blogs = await getBlogs();
+
+  return !blogs ? null : (
     <>
       <h1 className="page-title">Blog</h1>
       <div id="blog-container">
