d4cec0048d5380d1340e9dd50e478ed826d85bad
Sean Nguyen <smnguyen745@gmail.com>
2023-11-17 03:16:44 +0100

    feat: add fields to Blog interface

--- bootcamp-milestone-2/src/app/blogData.ts
+++ bootcamp-milestone-2/src/app/blogData.ts
@@ -1,22 +1,34 @@
+type IComment = {
+  user: string;
+  comment: string;
+  time: Date;
+};
+
 export interface Blog {
   title: string;
-  date: string;
+  date: Date;
   description: string;
   slug: string;
+  content: string; // for individual blog page
+  comments: IComment[]; // array for comments
 };
 
 const blogList: Blog[] = [
   {
     title: "Cinque Terre",
-    date: "10/21/2023",
+    date: new Date("2023-10-21"),
     description: "My first weekend trip exploring another part of Italy!",
     slug: "/blog/cinque_terre",
+    content: "",
+    comments: [],
   },
   {
     title: "Tuscany",
-    date: "10/21/2023",
+    date: new Date("2023-10-21"),
     description: "The program-organized event, covering Flourence and Pisa",
     slug: "/blog/tuscany",
+    content: "",
+    comments: [],
   },
 ];
 
