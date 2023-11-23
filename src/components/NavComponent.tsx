2c1015980d683b57030c5d333927c3533f6c5a74
noahgiboney <noahgiboney@gmail.com>
2023-11-22 17:11:27 -0800

    fix: nav bar routing using Link from nextJS

--- src/components/NavComponent.tsx
+++ src/components/NavComponent.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import style from "../styles/navbar.module.css";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
     <header className={style.navigation}>
     <ul> 
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><Link href={'/'}>Home</Link></li>
+        <li><Link href={'/#about'}>About</Link></li>
+        <li><Link href={'/#portfolio'}>Portfolio</Link></li>
+        <li><Link href={'/#contact'}>Contact</Link></li>
     </ul>
     </header>
   );
