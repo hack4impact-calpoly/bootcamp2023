8eb7946878df5e097308d64d1fc5913aa2bc48b6
Sriya-Adunur <sadunur@calpoly.edu>
2023-11-26 11:28:22 -0800

    updated blog 2 details

--- bootcamp-milestone-2/src/app/blog2/page.tsx
+++ bootcamp-milestone-2/src/app/blog2/page.tsx
@@ -3,11 +3,11 @@
 
   export default function Blogs() {
       return (<main>
-          <h1 className="page-title">The Competitive Space of Grace Hopper</h1>
+          <h1 className="page-title">Must Watch Halloween Movies</h1>
         <h2 className="section-title">Date: 10-24-2023</h2>
           <Image src= "/Halloween.png" alt="Halloween Movies." width="400" height="300" />
         <p className="entry-info">
-          Hi! This is my first blog post.
+          Hi! This is my second blog post.
         </p>
       </main>)
     }
