91879ef1d87a7408c1132b840d70bfcee013e855
Sriya-Adunur <sadunur@calpoly.edu>
2023-11-25 20:31:12 -0800

    completed blog 1

--- bootcamp-milestone-2/src/app/blog1/page.tsx
+++ bootcamp-milestone-2/src/app/blog1/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export default function Blogs() {
     return (<main>
         <h1 className="page-title">The Competitive Space of Grace Hopper</h1>
-      <h2 className="section-title">Date: 10-24-2023</h2>
-        <Image src= "/GraceHopper.jpeg" alt="Grace Hopper" width="400" height="300" />
+      <h2 className="section-title">Date: 10-25-2023</h2>
+        <Image src= "/GraceHopper.jpeg" alt="Grace Hopper" width="400" height="300"/>
       <p className="entry-info">
         Hi! This is my second blog post.
       </p>
