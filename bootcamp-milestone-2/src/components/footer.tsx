0519017ec610556d450d758e5065fca593dfbc9c
Sriya-Adunur <sadunur@calpoly.edu>
2023-11-26 13:23:58 -0800

    added footer

---
+++ bootcamp-milestone-2/src/components/footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./footer.module.css";
+
+export default function Footer() {
+    return (
+        <footer className={style.footer}>© 2023 Sriya Adunur's Personal Website | All Rights Reserved</footer>
+    );
+}
