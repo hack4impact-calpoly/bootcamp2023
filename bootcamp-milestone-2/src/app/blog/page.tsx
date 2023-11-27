d3d8a6640603e74c4d896def1a9c653c5d6b3961
Sean Nguyen <smnguyen745@gmail.com>
2023-11-27 01:32:49 +0100

    feat: backend connected to front end for blog

--- bootcamp-milestone-2/src/app/blog/page.tsx
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -1,28 +1,73 @@
 import style from "./blog.module.css";
 import blogs from "../blogData";
 import BlogPreview from "../../components/blogPreview";
+import connectDB from "../../helpers/db";
+import BlogComp from "../../database/blogSchema";
+import React, { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
 
-export default function Blog() {
-  return (
-    <div>
-      <main>
-        <h1 className={style.pageTitle}>Blog</h1>
-        <h2 className={style.pageSubtitle}>Posts</h2>
-        <div className={style.blogList} id={style.blogList}></div>
+async function getBlogs() {
+  const connection = await connectDB(); // function from db.ts before
 
-        {blogs.map((blog) => (
-          <BlogPreview
-            key={blog.title}
-            title={blog.title}
-            date={blog.date}
-            description={blog.description}
-            slug={blog.slug}
-            pic={"/cinque_terre_1.jpg"}
-          />
-        ))}
-      </main>
+  try {
+    // query for all blogs and sort by date
+    const blogs = await BlogComp.find().sort({ date: -1 }).orFail();
+    // send a response as the blogs as the message
+    return blogs;
+  } catch (err) {
+    return null;
+  }
+}
 
-      {/* <script src="./src/blog.js"></script> */}
+export default async function Blog() {
+  // const [blogList, setBlogList] = useState([]);
+
+  // useEffect(() => {
+  //   async function fetchBlogs() {
+  //     try {
+  //       const result = await getBlogs();
+  //       if (result != null) {
+  //         setBlogList(result);
+  //       }
+  //       else{
+  //         console.log("null");
+  //       }
+  //     } catch (err) {
+  //       console.error("Error fetching blogs:", err);
+  //     }
+  //   }
+
+  //   fetchBlogs();
+  // }, []);
+
+  const blogPosts = await getBlogs();
+
+  if (blogPosts) {
+    return (
+      <div>
+        <main>
+          <h1 className={style.pageTitle}>Blog</h1>
+          <h2 className={style.pageSubtitle}>Posts</h2>
+          <div className={style.blogList} id={style.blogList}></div>
+
+          {blogPosts.map((blog) => (
+            <BlogPreview
+              key={blog.title}
+              title={blog.title}
+              date={blog.date}
+              description={blog.description}
+              slug={blog.slug}
+              pic={"/cinque_terre_1.jpg"}
+            />
+          ))}
+        </main>
+
+        {/* <script src="./src/blog.js"></script> */}
+      </div>
+    );
+  } else {
+    <div className="flex flex-col gap-16">
+      <h1 className="text-2xl font-bold">No blog posts found</h1>
     </div>
-  );
+  }
 }
