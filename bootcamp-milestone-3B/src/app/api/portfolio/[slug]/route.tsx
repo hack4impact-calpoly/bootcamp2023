129c7f0a5806559f881ddd0735f422ab3592212e
ktaschek <ktaschek@gmail.com>
2023-11-27 23:28:28 -0800

    refactor: use api endpoints for dynamic routing of blog and project pages

---
+++ bootcamp-milestone-3B/src/app/api/portfolio/[slug]/route.tsx
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/helpers/db";
+import portfolioSchema from "@/database/portfolioSchema";
+
+type IParams = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function GET(req: NextRequest, { params }: IParams) {
+  await connectDB(); // function from db.ts before
+  const { slug } = params; // another destructure
+
+  try {
+    const project = await portfolioSchema.findOne({ slug }).orFail();
+    return NextResponse.json(project);
+  } catch (err) {
+    return NextResponse.json("Blog not found.", { status: 404 });
+  }
+}
