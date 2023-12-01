74ba13a52fbd8a354160ad568fc768ae7797e4a7
unknown <kaylatran2021@gmail.com>
2023-11-30 18:48:38 -0800

    feat: connecting backend - milestone 3B

--- bootcamp-milestone-2/src/app/blog/page.tsx
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -3,26 +3,46 @@ import BlogPreview from "../components/blogPreview";
 import blogSchema from "../database/blogSchema";
 import connectDB from "../database/db";
 
+async function getBlog() {
+  const res = await fetch("localhost:3000/api/blog/" + slug);
+  if (res.ok) {
+    return await res.json();
+  } else {
+    return null;
+  }
+}
+
 export default async function Blogs() {
-  const blogs = await getBlogs();
+  const blogs = await Page();
+  // const individualBlog = await getBlog();
   if (blogs == null) {
     return (
-      <div>
+      <main>
+        <h1>Blog</h1>
         <p>No blogs currently.</p>
-      </div>
+      </main>
     );
   } else {
     return (
-      <div>
-        {blogs.map((blog) => (
-          <BlogPreview {...blog} />
-        ))}
-      </div>
+      <main>
+        <div>
+          {blogs.map((blog) => (
+            <BlogPreview
+              title={blog.title}
+              description={blog.description}
+              date={JSON.stringify(blog.date)}
+              slug={blog.slug}
+              image={""}
+              content={""}
+            />
+          ))}
+        </div>
+      </main>
     );
   }
 }
 
-async function getBlogs() {
+async function Page() {
   await connectDB(); // function from db.ts before
 
   try {
