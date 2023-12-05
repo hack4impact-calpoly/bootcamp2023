4bbc71988664cf05368f2fdd647ad6f7789f5bc1
unknown <kaylatran2021@gmail.com>
2023-12-04 19:45:23 -0800

    feat: project stylings

--- bootcamp-milestone-2/src/app/components/project.tsx
+++ bootcamp-milestone-2/src/app/components/project.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import type { IProject } from "../database/projectSchema";
 import Image from "next/image";
 import Link from "next/link";
-// import style from "./projects.module.css";
+import style from "../portfolio/projects.module.css";
 
 export default function Projects(props: IProject) {
   return (
     <div>
+      <h1 className={style.pageTitle}>Blog</h1>
       <main>
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
-        <Link href={props.slug}><Image src={props.image} alt="" height={200} width={200}></Image></Link>
+        <h2 className={style.pageTitle}>{props.title}</h2>
+        <div className={style.projectImage}>
+          <Link href={props.slug}>
+            <Image src={props.image} alt="" height={150} width={250}></Image>
+          </Link>
+        </div>
+        <div className={style.projectDetails}>
+          <p className={style.projectDescription}>{props.description}</p>
+          <p className={style.projectMore}>Learn more</p>
+        </div>
       </main>
     </div>
   );
