3cbbc1416aefa8715732ee98ab062ffaf50995b7
Jonathan DeBarro <jdebarro@calpoly.edu>
2023-12-15 12:00:14 -0800

    Asynch function to get blogs from database

--- bootcamp-milestone-2/src/app/blog/page.tsx
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -2,7 +2,21 @@ import Image from 'next/image'
 import '../globals.css'
 import blogs from '../blogData';
 import BlogPreview from '@/components/blogPreview';
+import connectDB from '@/helpers/db';
+import Blog from '@/database/blogSchema';
 
+async function getBlogs() {
+    await connectDB() // function from db.ts before
+
+    try {
+        // query for all blogs and sort by date
+        const blogs = await Blog.find().sort({ date: -1 }).orFail()
+        // send a response as the blogs as the message
+        return blogs
+    } catch (err) {
+        return null
+    }
+}
 // You can name the function within page.tsx anything you want.
 export default function Home() {
   return (<div>
