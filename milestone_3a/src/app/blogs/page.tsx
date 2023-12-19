8697ea35c9e51a21b07d3d51ae36f7216dfd0122
Westluu <west1luu@gmail.com>
2023-12-19 14:53:03 -0800

    feat: connect mongo to frontend

---
+++ milestone_3a/src/app/blogs/page.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import blogs from "../blogData";
+import BlogPreview from "@/components/blogPreview";
+import connectDB from "../../helper/db";
+import Blogs from "../../database/blogSchema";
+
+async function getBlogs() {
+    await connectDB(); // function from db.ts before
+
+    try {
+        // query for all blogs and sort by date
+        const blogs = await Blogs.find().sort({ date: -1 }).orFail();
+        // send a response as the blogs as the message
+        return blogs;
+    } catch (err) {
+        return null;
+    }
+}
+
+export default function Blog() {
+    return getBlogs().then((blogs: any[])=> {
+        return (
+            <main>
+                <h1 className="blog">Blog</h1>
+                <div className="blog-container">
+                    {blogs.map((blog) => (
+                        <BlogPreview 
+                            key={blog.slug}
+                            title={blog.title}
+                            description={blog.description}
+                            slug={blog.slug}
+                            image={blog.image}
+                            date={blog.date}
+                        />
+                    ))}
+                </div>
+            </main>
+        );
+    });
+}
