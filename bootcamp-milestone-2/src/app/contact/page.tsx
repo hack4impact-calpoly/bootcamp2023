8839c80934d9e156cb479aba95183191d90be3c7
Sean Nguyen <smnguyen745@gmail.com>
2023-12-20 08:06:08 -0800

    feat: formatted contacts page to use buttons for Links

--- bootcamp-milestone-2/src/app/contact/page.tsx
+++ bootcamp-milestone-2/src/app/contact/page.tsx
@@ -1,51 +1,61 @@
+import Link from "next/link";
 import style from "./contact.module.css";
 
 export default function Contact() {
   return (
     <div>
-      {/*content goes here */}
-      <main>
+      <main className={style.page}>
         <h1 className={style.pageTitle}>Contact</h1>
-        <p>Email: snguy212@calpoly.edu</p>
-        <p>
-          Linkedin:
-          <a href="https://www.linkedin.com/in/seanmnguyen/">
-            https://www.linkedin.com/in/seanmnguyen/
-          </a>
-        </p>
-        <p>
-          GitHub:
-          <a href="https://github.com/seanmnguyen">
-            https://github.com/seanmnguyen
-          </a>
-        </p>
-        <form className="all" id={style.contactForm}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Name"
-            required
-          />
-          <br />
-          <br />
-          <label className="all" htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" placeholder="Email" />
-          <br />
-          <br />
-          <label className="all" htmlFor="message">Message</label>
-          <textarea
-          className="all"
-            id="message"
-            name="message"
-            placeholder="Message"
-            required
-          ></textarea>
-          <br />
-          <br />
-          <input type="submit" value="Submit" />
-        </form>
+        <div className={style.pageContent}>
+          <form className={style.contactForm}>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Name"
+              required
+            />
+            <br />
+            <br />
+            <label className="all" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              required
+            />
+            <br />
+            <br />
+            <label className="all" htmlFor="message">
+              Message
+            </label>
+            <textarea
+              className="all"
+              id="message"
+              name="message"
+              placeholder="Message"
+              required
+            ></textarea>
+            <br />
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+          <div className={style.links}>
+            <p>Email: snguy212@calpoly.edu</p>
+            <div className={style.linksButtons}>
+              <Link href="https://www.linkedin.com/in/seanmnguyen/">
+                LinkedIn
+              </Link>
+            </div>
+            <div className={style.linksButtons}>
+              <Link href="https://github.com/seanmnguyen">GitHub</Link>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
