30abc0c9ad04190bb869c9943a4cc059c64bf9f9
Aaron Ahmadyar <aaron.ahmadyar@gmail.com>
2023-12-20 16:32:38 -0800

    feat: setup contact form submit

--- milestone4/src/app/contact/page.tsx
+++ milestone4/src/app/contact/page.tsx
@@ -1,13 +1,44 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Blog() {
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        //get form submission event
+        const formElement = e.target as HTMLFormElement;
+
+        //Collect form submission data
+        const nameInput =
+            formElement.querySelector<HTMLInputElement>(
+                'input[name="name"]'
+            )?.value;
+        const emailInput = formElement.querySelector<HTMLInputElement>(
+            'input[name="email"]'
+        )?.value;
+        const descriptionText = formElement.querySelector<HTMLTextAreaElement>(
+            'textarea[name="description"]'
+        )?.value;
+
+        //Store form submission data in single JSON to be submitted
+        const data = {
+            name: nameInput,
+            email: emailInput,
+            description: descriptionText,
+        };
+
+        //Send form data to endpoint
+        const response = axios.post("/api/contact", data);
+    }
+
     return (
         <main>
             <h1 className="contact">Contact</h1>
-            <form id="contact-form">
-                <input type="text" id="name" placeholder="Name" />
-                <input type="email" id="email" placeholder="email" />
-                <textarea></textarea>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <input type="text" name="name" placeholder="Name" required />
+                <input type="email" name="email" placeholder="email" required />
+                <textarea name="description" required></textarea>
                 <input type="submit" />
             </form>
         </main>
