d5358eaa3d5ab34df1f8d7741a647355ff897a74
Sophia Peckner <sophiapeckner@gmail.com>
2023-12-20 20:34:39 -0800

    feat: add image attribute and append element with styling

--- src/blog.js
+++ src/blog.js
@@ -1,21 +1,22 @@
 var blogs = [
     {
-        title: "Graduation",
+        title: "Graduation: end of an era",
         date: "05-30-2023",
         description: "Looking back on the last four",
-        slug: ""
+        image: "images/graduation.jpeg"
     },
     {
         title: "Volunteering in Taiwan",
         date: "08-04-2023",
         description: "How I spent my summer in Taiwan",
-        slug: ""
+        image: "images/taiwan.JPG"
     }
 ];
 var container = document.getElementsByClassName("blogs");
 blogs.forEach(function (blog) {
     var blogPost = document.createElement("div");
-    blogPost.innerHTML = "\n        <h1>".concat(blog.title, "</h1>\n        <h2>").concat(blog.date, "</h2>\n        <p>").concat(blog.description, "</p>\n    ");
+    blogPost.classList.add("bordered_container");
+    blogPost.innerHTML = "\n        <div>\n            <h1>".concat(blog.title, "</h1>\n            <p>").concat(blog.description, "</p>\n            <h2>").concat(blog.date, "</h2>\n        </div>\n        <img src=\"").concat(blog.image, "\" width=\"400\" height=\"300\">\n    ");
     // container[0] is the first occurence of element with class="blogs"
     container[0].appendChild(blogPost);
 });
--- src/blog.ts
+++ src/blog.ts
@@ -2,21 +2,21 @@ type Blog = {
     title: string;
     date: string;
     description: string;
-    slug: string;
+    image: string;
 }
 
 const blogs: Blog[] = [
     {
-        title: "Graduation",
+        title: "Graduation: end of an era",
         date: "05-30-2023",
         description: "Looking back on the last four",
-        slug: ""
+        image: "images/graduation.jpeg"
     },
     {
         title: "Volunteering in Taiwan",
         date: "08-04-2023",
         description: "How I spent my summer in Taiwan",
-        slug: ""
+        image: "images/taiwan.JPG"
     }
 ]
 
@@ -24,11 +24,15 @@ var container = document.getElementsByClassName("blogs");
 
 blogs.forEach((blog) => {
     var blogPost = document.createElement("div")
+    blogPost.classList.add("bordered_container")
 
     blogPost.innerHTML = `
-        <h1>${blog.title}</h1>
-        <h2>${blog.date}</h2>
-        <p>${blog.description}</p>
+        <div>
+            <h1>${blog.title}</h1>
+            <p>${blog.description}</p>
+            <h2>${blog.date}</h2>
+        </div>
+        <img src="${blog.image}" width="400" height="300">
     `
     
     // container[0] is the first occurence of element with class="blogs"
