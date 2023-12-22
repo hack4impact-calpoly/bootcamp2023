6b4f91a61e431c3cb4553c749bc3093e09656c8c
Sean Nguyen <smnguyen745@gmail.com>
2023-12-22 02:29:00 -0800

    feat: editing next.configs.js

--- bootcamp-milestone-2/next.config.js
+++ bootcamp-milestone-2/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.export = {
     async headers() {
     return [
       {
@@ -8,7 +8,7 @@ module.exports = {
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
       }
