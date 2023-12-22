81b4e7728642a60bc451b03c6d1028e3bb447acc
Sumedha Kundurthi <70816655+SumedhaKun@users.noreply.github.com>
2023-12-22 14:00:38 -0800

    Added env-variable URL

--- bootcamp-milestone-3A/next.config.js
+++ bootcamp-milestone-3A/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     env: {
         MONGO_URI:"mongodb+srv://SumedhaKun:newPassword@cluster0.dsupf6p.mongodb.net/?retryWrites=true&w=majority",
-        URL:"https://bootcamp-project-2023-bbgd.vercel.app/"
+        URL:"https://bootcamp-project-2023-bbgd-git-main-sumedha-kundurthis-projects.vercel.app"
       },
 }
 
