1ce1a2f08310e99a4d8371a91ca8f728edfe2953
Sophia Peckner <sophiapeckner@gmail.com>
2023-12-22 10:16:10 -0800

    change folder name to bootcamp-milestones

---
+++ bootcamp-milestones/src/database/blogSchema.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+// typescript type (can also be an interface)
+export type IBlog = {
+    title: string;
+    slug: string; 
+    date: Date;
+    description: string; // for preview
+    content: string; // for individual blog page
+    image: string;
+};
+
+
+// mongoose schema 
+const blogSchema = new Schema<IBlog>({
+    title: { type: String, required: true },
+    slug: { type: String, required: true },
+    date: { type: Date, required: false, default: new Date()},
+    description: { type: String, required: true },
+    content: { type: String, required: true },
+    image: { type: String },
+})
+
+// defining the collection and model
+const Blogs = mongoose.models['blogs'] || 
+    mongoose.model('blogs', blogSchema);
+
+export default Blogs;
