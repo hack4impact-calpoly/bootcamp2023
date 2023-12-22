f61536dee9ca073eccf3c9891c04075e6a55a418
Aidan Nesbitt <aidannesbitt20@gmail.com>
2023-12-22 06:44:19 -0800

    fix: added a rewrite to next.config.js so that startup page of website reroutes to /home (i'm not sure if this is best practice

--- bootcamp-milestone-2/next.config.js
+++ bootcamp-milestone-2/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
