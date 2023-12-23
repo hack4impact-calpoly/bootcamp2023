863381df25a3af3b82bae24f3253ec182cd776e3
Sophia Peckner <sophiapeckner@gmail.com>
2023-12-22 21:36:13 -0800

    milestone 4: POST api endpoint for comments; tried multiple findOneAndUpdate operations

---
+++ bootcamp-milestones/src/app/api/blog/[slug]/comment/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server'
+import connectDB from "@/helpers/db"
+import Blogs from "@/database/blogSchema";
+
+type IParams = {
+		params: {
+			slug: string
+		}
+}
+
+export async function POST(req: NextRequest, { params }: IParams) {
+    const body = await req.json();
+    const { slug } = params;
+    
+    try {
+        await connectDB();
+        const blog = await Blogs.findOneAndUpdate({ slug }, {$addToSet: {test_arr: "b"}})
+        return NextResponse.json(blog);
+    } catch (err) {
+        return NextResponse.json("Blog not found.", { status: 404 });
+    }
+}
