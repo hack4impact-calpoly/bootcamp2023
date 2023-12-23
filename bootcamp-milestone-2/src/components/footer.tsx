6ada1f853ebfd02b32e4b48306c2e02e6e974791
brandonwDiaryz <legobw1018@gmail.com>
2023-12-22 23:01:19 -0800

    Edit wording on footer

--- bootcamp-milestone-2/src/components/footer.tsx
+++ bootcamp-milestone-2/src/components/footer.tsx
@@ -3,7 +3,7 @@ import style from "./footer.module.css"
 export default function Footer() {
     return (
         <>
-            <footer className={style.footer}>© 2023 wbrandon25's Personal Website | All Rights Reserved</footer>
+            <footer className={style.footer}>2023 wbrandon25s Personal Website - All Rights Reserved</footer>
         </>
     )
 }
