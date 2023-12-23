98d65b0709eca83e0cc172ae2b72d4aae82cc2fe
Sriya-Adunur <sadunur@calpoly.edu>
2023-12-22 21:04:30 -0800

    project preview

---
+++ bootcamp-milestone-2/src/components/projectPreview.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import Link from "next/link";
+import React from 'react';
+import { IProject } from '@/database/projectSchema';
+
+export default function ProjectPreview(props: IProject) {
+    //console.log('ProjectPreview Slug:', props); 
+    return (
+      <div className = "projects">
+         <h2 style={{ textAlign: 'center' }}>
+        <Link href={props.slug}>{props.title}</Link>
+         </h2>
+         <h3>{props.description}</h3>
+        <div>
+        <Image className="project-image" src= {props.image} alt="Screenshot" width= {4000} height= {4000} ></Image>
+        </div>
+      </div>
+    );
+  }
