360064ca39cb3b8e095a43d3cb341fbfa20d0677
Sriya-Adunur <sadunur@calpoly.edu>
2023-12-22 21:09:23 -0800

    blog updates for 3a

--- bootcamp-milestone-2/src/app/blog/page.tsx
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -1,18 +1,41 @@
 import React from "react";
 import BlogPreview from '@/components/blogPreview';
-import blogs from "../blogData";
+import connectDB from "@/database/helpers/db";
+import Blogs from "@/database/blogSchema";
 
-export default function Blog() {
-    return(
+
+async function getBlogs()
+{
+	await connectDB() // function from db.ts before
+
+	try {
+			// query for all blogs and sort by date
+	    const blogs = await Blogs.find().sort({ date: -1 }).orFail()
+			// send a response as the blogs as the message
+	    return blogs
+	} catch (err) 
+    {
+        return null
+	}
+}
+
+export default async function Blog(){
+    const blogs = await getBlogs();
+    //console.log('Blogs:', blogs);
+
+    return (
     <main>
         <h2 className="page-title">Blogs</h2>
         <div>
-            {blogs.map(blog => 
-                <BlogPreview  {...blog} />
+            {blogs?.map(blog => 
+                <BlogPreview  title={blog.title}
+                slug = {blog.slug}
+                date = {(blog.date)}
+                description={blog.description}
+                content={blog.content}
+                comments={blog.comments} />
 		    )}
         </div>
     </main>
-
     );
-
-}
+    };
--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import type { Blog } from "@/app/blogData";
+import { IBlog } from '@/database/blogSchema';
 import Link from "next/link";
 
-
-export default function BlogPreview(props: Blog) {
-    return (
-      <div className = "bloglist">
-        <h2><Link href={props.slug}> {props.title} </Link></h2>
-        <div>
-          <p>{props.date}</p>
-          <p>{props.description}</p>
-        </div>
+export default function BlogPreview(props: IBlog) {
+  //console.log('BlogPreview Slug:', props); 
+  return (
+    <div className = "bloglist">
+      <h2><Link href = {props.slug}> {props.title} </Link></h2>
+      <div>
+      <p>{props.date ? props.date.toDateString() : 'No date available'}</p>
+        <p>{props.description}</p>
       </div>
-    );
-  }
+    </div>
+  );
+}
+//      <h2><Link href = {`blog/${props.slug}`}> {props.title} </Link></h2>
