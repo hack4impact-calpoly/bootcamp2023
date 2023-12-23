b2cd8bd482194e50fb7a4603c7d4a3cef061ab62
Sophia Peckner <sophiapeckner@gmail.com>
2023-12-22 21:39:37 -0800

    add contact page to navbar

--- bootcamp-milestones/src/components/navbar.tsx
+++ bootcamp-milestones/src/components/navbar.tsx
@@ -7,10 +7,11 @@ export default function Navbar() {
     // with your navbar code from your earlier milestones
     <nav>
         <ul className={style.ul}>
-        <li className={style.li}><a className={style.a} href="/blog">BLOG</a></li>
-        <li className={style.li}><a className={style.a} href="/resume">RESUME</a></li>
-        {/* <li className={style.li}><a className={style.a} href="#projects">PROJECTS</a></li> */}
-        <li className={style.li}><a className={style.a} href="/">HOME</a></li>
+          <li className={style.li}><a className={style.a} href="/contact">CONTACT</a></li>
+          <li className={style.li}><a className={style.a} href="/blog">BLOG</a></li>
+          <li className={style.li}><a className={style.a} href="/resume">RESUME</a></li>
+          {/* <li className={style.li}><a className={style.a} href="#projects">PROJECTS</a></li> */}
+          <li className={style.li}><a className={style.a} href="/">HOME</a></li>
         </ul>
     </nav>
   );
