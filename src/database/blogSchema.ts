c627ea1dcf59a84e7607b3bedbde7d5635ea8a57
Sohini-pillay <sohinispillay@gmail.com>
2023-12-29 21:49:52 -0800

    feat: fix

--- src/database/blogSchema.ts
+++ src/database/blogSchema.ts
@@ -2,35 +2,37 @@ import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
 export type IComment = {
-    user: string;
-    comment: string;
-    time: Date;
-}
-
-export type IBlog = {
-    title: string;
-    slug: string; 
-    date: Date;
-    description: string; // for preview
-    content: string; // for individual blog page
-    comments: IComment[]; // array for comments
+  user: string;
+  comment: string;
+  time: Date;
 };
 
-// mongoose schema 
+export type IBlog = {
+  _id: string;
+  title: string;
+  slug: string;
+  date: Date;
+  description: string; // for preview
+  content: string; // for individual blog page
+  comments: IComment[]; // array for comments
+};
+
+// mongoose schema
 const blogSchema = new Schema<IBlog>({
-    title: { type: String, required: true },
-    slug: { type: String, required: true },
-    date: { type: Date, required: false, default: new Date()},
-    description: { type: String, required: true },
-    content: { type: String, required: true },
-    comments: {
-        user: {type: String, required: false},
-        comment: {type: String, required: false},
-        time: {type: Date, required: false, default: new Date()}
-    }
-})
+  _id: { type: String, required: true },
+  title: { type: String, required: true },
+  slug: { type: String, required: true },
+  date: { type: Date, required: false, default: new Date() },
+  description: { type: String, required: true },
+  content: { type: String, required: true },
+  comments: {
+    user: { type: String, required: false },
+    comment: { type: String, required: false },
+    time: { type: Date, required: false, default: new Date() },
+  },
+});
 
 // defining the collection and model
-const Blogs = mongoose.models['blogs'] || mongoose.model('blogs', blogSchema);
+const Blogs = mongoose.models["blogs"] || mongoose.model("blogs", blogSchema);
 
 export default Blogs;
