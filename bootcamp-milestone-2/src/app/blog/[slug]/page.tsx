d4ccc7da8a6b14fb47b7d5fdf455071547cfe31f
Sriya-Adunur <sadunur@calpoly.edu>
2023-12-31 13:08:40 -0800

    blog page changes

---
+++ bootcamp-milestone-2/src/app/blog/[slug]/page.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Image from "next/image";
+import Comment from "@/components/commentPreview";
+import connectDB from "@/database/helpers/db";
+import AddComment from "@/components/addComment";
+
+type Props = {
+    params: { slug: string };
+  };
+  
+  async function getBlogs(slug: string) {
+    await connectDB(); // function from db.ts before
+  
+    try {
+        const res = await fetch(`http://localhost:3000/api/blog/${slug}`, {
+        cache: "no-store",
+      });
+  
+      //console.log(slug);
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch blog");
+      }
+  
+      return res.json();
+    } catch (err: unknown) {
+      console.log(`error: ${err}`);
+      return null;
+    }
+  }
+
+  export default async function Blog({ params: { slug } }: Props) {
+    const blog = await getBlogs(slug);
+    console.log(slug);
+
+    if (blog) {
+
+      return (
+        <main>
+            <h1 className="page-title">{blog.title}</h1>
+            <h2 className="date">{new Date(blog.date).toLocaleDateString()}</h2>
+            <div className="blog-entry">
+            <h2 className="section-title" style={{ textAlign: 'center' }}>
+            </h2>            
+            <Image src={blog.image} alt={blog.title} width={400} height={300} />
+            </div>
+            <p className="entry-info">{blog.content}</p>
+
+            <h2>Comments</h2>
+            {blog.comments.map((comment: any, index: number) => (
+            <Comment key={index} comment={comment} />
+            ))} 
+            <div>
+              <h2>Add a Comment!</h2>
+              <AddComment slug={`blog/${slug}`} />
+            <br></br>
+          </div>
+            </main>
+      );
+    } else {
+      return (
+        <main>
+          <h1>Blog Not Found</h1>
+        </main>
+      );
+    }
+  }
