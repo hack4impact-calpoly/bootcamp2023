d9d75532a5a7c56322738d2f22b8bb890decb459
Sriya-Adunur <sadunur@calpoly.edu>
2023-12-31 12:56:13 -0800

    project edits

--- bootcamp-milestone-2/src/database/projectSchema.ts
+++ bootcamp-milestone-2/src/database/projectSchema.ts
@@ -1,10 +1,21 @@
 import mongoose, { Schema } from "mongoose";
+// import { IComment } from "./blogSchema";
 
-type IComment = {
-    user: string;
-    comment: string;
-    time: string;
-}
+// const projectCommentSchema = new mongoose.Schema<IComment>({
+//     user: {
+//       type: String,
+//       required: true,
+//     },
+//     comment: {
+//       type: String,
+//       required: true,
+//     },
+//     time: {
+//       type: Date,
+//       required: false,
+//       default: new Date(),
+//     },
+//   });
 
 // typescript type (can also be an interface)
 type IProject = {
@@ -12,7 +23,6 @@ type IProject = {
     slug: string;
     description: string; // for preview
     image: string;
-    comments: IComment[]; // array for comments
 };
 
 // mongoose schema 
@@ -20,12 +30,7 @@ const projectSchema = new Schema<IProject>({
     title: { type: String, required: true },
     slug: { type: String, required: true },
     description: { type: String, required: true },
-    image: { type: String, required: true },
-    comments: {
-        user: {type: String, required: true},
-        comment: {type: String, required: true},
-        time: {type: String, required: false, default: new Date()}
-    }
+    image: { type: String, required: true }
 })
 
 // defining the collection and model
@@ -33,5 +38,9 @@ const Projects = mongoose.models['projects'] ||
 mongoose.model('projects', projectSchema);
 
 export default Projects;
-export type { IComment };
+
+// export const ProjectComment =
+//   mongoose.models["projectcomment"] ||
+//   mongoose.model("projectcomment", projectCommentSchema);
+  
 export type { IProject };
