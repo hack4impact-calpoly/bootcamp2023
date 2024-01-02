828a01c1aa59d63931acf55038b1f6595a66432a
Sriya-Adunur <sadunur@calpoly.edu>
2024-01-02 00:38:52 -0800

    added stylings

--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { IBlog } from '@/database/blogSchema';
 import Link from "next/link";
+import styles from "./blogPreview.module.css";
+
 
 export default function BlogPreview(props: IBlog) {
   return (
-    <div className = "bloglist">
+    <div className = {styles.bloglist}>
       <h2><Link href = {`/blog/${props.slug}`}> {props.title} </Link></h2>
       <div>
       <p>{props.date ? props.date.toDateString() : 'No date available'}</p>
