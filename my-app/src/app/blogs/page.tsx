b2501d0ec7a67b92e08799485bceb9bb73d05440
Melika Dabiri <jalehdabiri@gmail.com>
2024-01-05 17:41:05 -0800

    fixed compilation errors - eslint rules update + key attribute when iterating

--- my-app/src/app/blogs/page.tsx
+++ my-app/src/app/blogs/page.tsx
@@ -1,54 +1,3 @@
-// import connectDB from "@/helpers/db";
-// import BlogModel from "@/database/blogSchema";
-// import BlogPreview from "@/components/blogPreview";
-// import Image from "next/image";
-
-
-
-// export default function Blogs() {
-
-//     async function getBlogs() {
-//         await connectDB(); // function from db.ts before
-      
-//         try {
-//           // query for all blogs and sort by date
-//           const blogs = await BlogModel.find().sort({ date: -1 }).orFail();
-//           console.log(blogs);
-//           // send a response as the blogs as the message
-//           return blogs;
-//         } catch (err) {
-//           return null;
-//         }
-//       }
-
-//     return(
-//         <div>
-//             <main className="index-main">
-//             <h1 className="index-title">Personal Blogs</h1>
-//             <div className="about">
-//                 {getBlogs().then(
-//                     (blogs) =>
-//                     blogs &&
-//                     blogs.map((blog) => (
-//                         <BlogPreview
-//                         title={blog.title}
-//                         date={blog.date}
-//                         description={blog.description}
-//                         slug={blog.slug}
-//                         content={blog.content}
-//                         comments={blog.comments}
-//                         ></BlogPreview>
-//                     ))
-//                 )}
-//             </div>
-    
-//             </main>
-//         </div>
-//     )
-
-// }
-
-
 import React from "react";
 import BlogPreview from "@/components/blogPreview";
 import blogSchema from "@/database/blogSchema";
@@ -73,6 +22,7 @@ export default async function Blogs() {
           <h1 className={style.pageTitle}>Blogs</h1>
           {blogs.map((blog) => (
             <BlogPreview
+              key={blog.title}
               title={blog.title}
               description={blog.description}
               date={blog.date}
