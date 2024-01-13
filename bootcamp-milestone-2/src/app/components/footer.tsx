edecbebb7e9ba61c0364b130ad69906d622441de
Amanda Chan <achan214@calpoly.edu>
2024-01-12 21:07:03 -0800

    fix: apostrophe vercel problem

--- bootcamp-milestone-2/src/app/components/footer.tsx
+++ bootcamp-milestone-2/src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
     return(
         <footer className="footer">
-        © 2023 Amanda Chan's Website | All Rights Reserved
+        © 2023 Amanda Chan&lsquo;s Website | All Rights Reserved
         </footer>
     );
 
