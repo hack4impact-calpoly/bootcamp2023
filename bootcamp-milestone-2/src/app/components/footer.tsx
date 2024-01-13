26f49a2a7632ea6058447a55abee8a952092d06f
Amanda Chan <achan214@calpoly.edu>
2024-01-12 20:58:25 -0800

    feat: created footer component lol

---
+++ bootcamp-milestone-2/src/app/components/footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+    return(
+        <footer className="footer">
+        © 2023 Amanda Chan's Website | All Rights Reserved
+        </footer>
+    );
+
+};
