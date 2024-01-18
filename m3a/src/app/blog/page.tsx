29e7b1e5c3b62c19b9dde9339d5751239c42f0a6
Ida Voong <idavoong@gmail.com>
2024-01-17 19:04:37 -0800

    vercel error

--- m3a/src/app/blog/page.tsx
+++ m3a/src/app/blog/page.tsx
@@ -1,40 +1,39 @@
 import React from "react";
 import BlogPreview from "../components/blogPreview";
-//import blogs from "../blogData";
 import connectDB from "../helpers/db";
 import BlogDB, { IBlog } from "../database/blogSchema";
 
 async function getBlogs() {
-  await connectDB(); // function from db.ts before
+  await connectDB() // function from db.ts before
 
   try {
     // query for all blogs and sort by date
     const blogs = await BlogDB.find().sort({ date: -1 }).orFail()
     // send a response as the blogs as the message
-    return blogs;
+    return blogs
   } catch (err) {
-    return null;
+    return null
   }
 }
 
 async function Blog() {
-  const blogsDB = await getBlogs()
+  const blogsDB = await getBlogs();
 
-    return (
-      <div>
-        <h1 className="page-title">Favorite Restaurants</h1>
-        <div className="blog" id="blog">
-          {blogsDB?.map((blog : IBlog) => (
-            // eslint-disable-next-line react/jsx-key
-            <BlogPreview
-              title={blog.title}
-              slug={blog.slug}
-              description={blog.description}
-            />
-          ))}
-        </div>
+  return (
+    <div>
+      <h1 className="page-title">Favorite Restaurants</h1>
+      <div className="blog" id="blog">
+        {blogsDB?.map((blog: IBlog) => (
+          // eslint-disable-next-line react/jsx-key
+          <BlogPreview
+            title={blog.title}
+            slug={blog.slug}
+            description={blog.description}
+          />
+        ))}
       </div>
-    );
-};
+    </div>
+  );
+}
 
 export default Blog;
